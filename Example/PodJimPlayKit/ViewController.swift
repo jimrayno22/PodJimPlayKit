984d3f349597637e9ca4a91b06c7af6313077833
jimrayno22 <jimrayno22@gmail.com>
2020-09-16 16:47:19 +0800

    Initial commit

---
+++ Example/PodJimPlayKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PodJimPlayKit
+//
+//  Created by jimrayno22 on 09/16/2020.
+//  Copyright (c) 2020 jimrayno22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
